db3677fac8eb889b7d826486286593270d5e8a8a
Zack <zack.c.campbell@gmail.com>
2018-09-07 21:17:16 -0500

    Added Fischer's Algorithm

--- src/ReferenceDoc.java
+++ src/ReferenceDoc.java
@@ -316,3 +316,31 @@ class Bakery implements Lock {
         number[i] = 0;
     }
 }
+
+/**
+ * Fischer's mutual exclusion algorithm
+ */
+class Fischer implements Lock {
+    int N;
+    int turn;
+    int delta;
+    public Fischer(int numProc) {
+        N = numProc;
+        turn = -1;
+        delta = 5;
+    }
+    public void requestCS(int i) {
+        while (true) {
+            while (turn != -1) {
+                turn = i;
+                try {
+                    Thread.sleep(delta);
+                } catch (InterruptedException e) {}
+                if (turn == i) return;
+            }
+        }
+    }
+    public void releaseCS(int i) {
+        turn = -1;
+    }
+}
