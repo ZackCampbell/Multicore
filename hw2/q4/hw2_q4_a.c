07b714a87d4bac864813a42dc2361228fcede7f2
Zack <zack.c.campbell@gmail.com>
2018-09-24 14:18:07 -0500

    Added makelists and reformatted C files

--- hw2/q4/hw2_q4_a.c
+++ hw2/q4/hw2_q4_a.c
@@ -3,18 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void MatrixMult(char file1[],char file2[],int T)
-{
-//Write your code here
+void MatrixMult(char file1[],char file2[],int T) {
+    //Write your code here
 }
 
-void main(int argc, char *argv[])
-{
-  char *file1, *file2;
-  file1=argv[1];
-  file2=argv[2];
-  int T=atoi(argv[3]);
-  MatrixMult(file1,file2,T);
+void main(int argc, char *argv[]) {
+    char *file1, *file2;
+    file1=argv[1];
+    file2=argv[2];
+    int T=atoi(argv[3]);
+    MatrixMult(file1,file2,T);
 }
 
 
--- hw2/q4/hw2_q4_b.c
+++ hw2/q4/hw2_q4_b.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <omp.h>
 
-double MonteCarlo(int s)
-{
-//Write your code here
+double MonteCarlo(int s) {
+    return 0.0;
+    //Write your code here
 }
 
-void main()
-{
-double pi;
-pi=MonteCarlo(100000000);
-printf("Value of pi is: %lf\n",pi);
+void main() {
+    double pi;
+    pi=MonteCarlo(100000000);
+    printf("Value of pi is: %lf\n",pi);
 }
 
 
