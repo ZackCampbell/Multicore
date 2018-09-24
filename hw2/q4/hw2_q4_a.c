51f53bd0363ba312155586f9697c9053e2eb37fc
Zack <zack.c.campbell@gmail.com>
2018-09-24 14:35:54 -0500

    Updated CMakeLists and got 4B to run

--- hw2/q4/hw2_q4_a.c
+++ hw2/q4/hw2_q4_a.c
@@ -7,12 +7,12 @@ void MatrixMult(char file1[],char file2[],int T) {
     //Write your code here
 }
 
-void main(int argc, char *argv[]) {
-    char *file1, *file2;
-    file1=argv[1];
-    file2=argv[2];
-    int T=atoi(argv[3]);
-    MatrixMult(file1,file2,T);
-}
+//void main(int argc, char *argv[]) {
+//    char *file1, *file2;
+//    file1=argv[1];
+//    file2=argv[2];
+//    int T=atoi(argv[3]);
+//    MatrixMult(file1,file2,T);
+//}
 
 
