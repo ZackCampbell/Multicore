12bcb34bbdeab6026320f9456d2cbcb5c08068a3
Paul Heath <pajheath@yahoo.com>
2018-09-24 21:59:29 -0500

    completed #3, thoroughly analyzed but not thorougly tested

--- hw2/q3/Monkey.java
+++ hw2/q3/Monkey.java
@@ -1,17 +1,58 @@
 package q3;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Monkey {
-
+	// declare the variables here
+	final static Lock lock = new ReentrantLock();
+	final Condition safeToClimb = lock.newCondition();
+	
+	volatile static int numMonkeys = 0;
+	volatile static int dirMonkeys = 0;
+	volatile static boolean kongCrossing = false;
+	volatile static long kongID = -1;	
+	
     public Monkey() {
-
     }
-
+    
+	// A monkey calls the method when it arrives at the river bank and wants to climb
+	// the rope in the specified direction (0 or 1); Kong’s direction is -1.
+	// The method blocks a monkey until it is allowed to climb the rope.
     public void ClimbRope(int direction) throws InterruptedException {
-
+    	lock.lock();
+    	boolean isKong = false;
+    	if(direction == -1)
+    		isKong = true;
+    	if(isKong) {
+    		kongCrossing = true;
+    		kongID = Thread.currentThread().getId();
+    		while(numMonkeys != 0) safeToClimb.await();
+    		++numMonkeys;
+    	} else {
+    		while((dirMonkeys != direction && numMonkeys != 0) || (dirMonkeys == direction && numMonkeys == 3) || kongCrossing) safeToClimb.await();
+    		++numMonkeys;
+    		if(dirMonkeys != direction) {
+    			dirMonkeys = direction;
+    		}
+    	}
+    	lock.unlock();
     }
 
+	// After crossing the river, every monkey calls this method which
+	// allows other monkeys to climb the rope.
     public void LeaveRope() {
-
+    	lock.lock();
+    	if(kongCrossing) {
+    		if(kongID == Thread.currentThread().getId()) {
+	    		kongCrossing = false;
+	    		kongID = -1;
+    		}
+    	}
+    	--numMonkeys;
+    	safeToClimb.signalAll();
+    	lock.unlock();
     }
 
     /**
@@ -24,7 +65,7 @@ public class Monkey {
      * case 2: when Kong is on the rope, this value should be 1
      */
     public int getNumMonkeysOnRope() {
-        return -1;
+        return numMonkeys;
     }
 
 }
