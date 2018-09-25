7b43652e9159fd9bb14bcfbcd864132ee7378774
Zack <zack.c.campbell@gmail.com>
2018-09-24 22:01:46 -0500

    Working on Q4b

--- hw2/q4/hw2_q4_b.c
+++ hw2/q4/hw2_q4_b.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <stdint.h>
+
+struct point {
+    int x;
+    int y;
+};
 
 double MonteCarlo(int s) {
-    return 0.0;
+    double radius = 100.0;
+    int circlePoints;
+
+    for (int i = 0; i < s; i++) {       // For every point
+        if () {                         // If the point is in the circle
+            // Increment circlePoints
+        }
+    }
+
+    // Block threads here
+    return (double)circlePoints * 4 / s;
     //Write your code here
 }
 
