eb67c1f409ee99b065065c5a17894551d53276ee
pheath <pajheath@yahoo.com>
2018-09-26 00:36:26 -0500

    4a works but not parallelized yet

--- hw2/q4/hw2_q4_a.c
+++ hw2/q4/hw2_q4_a.c
@@ -3,55 +3,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printMatrix(double matrix[], int matrixLength) {
-    for (int i = 0; i < matrixLength; i++) {
-        printf("%f ", matrix[i]);
-    }
+void printMatrix(int rs, int cs, double** matrix) {
+	printf("%d %d \n", rs, cs);
+	for(int i = 0; i < rs; i++) {
+	    for(int j = 0; j < cs; j++) {
+		printf("%lf  ", matrix[i][j]);
+		if(j == cs - 1) printf("\n");
+	    }
+	}
 }
 
 void MatrixMult(char file1[],char file2[],int T) {
-    FILE *inputFile1 = fopen(file1, "r");
-    FILE *inputFile2 = fopen(file2, "r");
-    int width1, height1, width2, height2;
+    FILE* inputFile1 = fopen(file1, "r");
+    FILE* inputFile2 = fopen(file2, "r");
     if (inputFile1 == NULL || inputFile2 == NULL) {
         printf("No such file");
         return;
     }
-    fscanf(inputFile1, "%d %d", &width1, &height1);
-    fscanf(inputFile2, "%d %d", &width2, &height2);
-    double matrix[width1][height2];
-
-    double** matrix2 = malloc(width2 * sizeof(double*));
-    for (int i = 0; i < height2; ++i)
-        matrix2[i] = malloc(sizeof(double));
-
-    for (int i = 0; i < height2; i++) {
-        for (int j = 0; j < width2; j++) {
-            fscanf(inputFile2, "%lf", &matrix2[i][j]);
+    int row1, col1, row2, col2;
+    fscanf(inputFile1, "%d %d", &row1, &col1);
+    fscanf(inputFile2, "%d %d", &row2, &col2);
+    double** matrix1 = malloc(row1 * sizeof(double*));
+    double** matrix2 = malloc(row2 * sizeof(double*));
+    for (int i = 0; i < row1; i++) {
+	matrix1[i] = malloc(col1 * sizeof(double));
+        for (int j = 0; j < col1; j++) {
+            fscanf(inputFile1, "%lf", &matrix1[i][j]);
         }
     }
-
-    for (int i = 0; i < height2; i++) {
-        for (int j = 0; j < width1; j++) {
-            double fromFile1, fromFile2 = matrix2[j][i];
-            fscanf(inputFile1, "%lf", &fromFile1);
-            matrix[i][j] = fromFile1 * fromFile2;
-        }
-        printMatrix(matrix[i], width1);
-        printf("\n");
+    for(int i = 0; i < row2; i++) {
+	matrix2[i] = malloc(col2 * sizeof(double));
+        for(int j = 0; j < col2; j++) {
+	    fscanf(inputFile2, "%lf", &matrix2[i][j]);
+	}
     }
-
-    for (int i = 0; i < height2; i++) {
-        free(matrix2[i]);
-    }
-    free(matrix2);
-
     fclose(inputFile2);
     fclose(inputFile1);
-    //Write your code here
+    int row3 = row1;
+    int col3 = col2;
+    double** result = malloc(row3 * sizeof(double*));
+    for(int i = 0; i < row3; i++) {
+	result[i] = malloc(col3 * sizeof(double));
+    }
+    for(int i = 0; i < row3; i++) {
+	for(int j = 0; j < col3; j++) {
+	    result[i][j] = 0;
+	    for(int k = 0; k < col1; k++) {
+		result[i][j] += (matrix1[i][k] * matrix2[k][j]);
+	    }
+	}
+    }
+    printMatrix(row3, col3, result);
+    for(int i = 0; i < row1; i++) {
+        free(matrix1[i]);
+    }
+    for(int i = 0; i < row2; i++) {
+	free(matrix2[i]);
+    }
+    for(int i = 0; i < row3; i++) {
+	free(result[i]);
+    }
+    free(matrix1);
+    free(matrix2);
+    free(result);
 }
-
-
 
 void main(int argc, char *argv[]) {
     char *file1, *file2;
@@ -60,5 +75,4 @@ void main(int argc, char *argv[]) {
     int T=atoi(argv[3]);
     MatrixMult(file1,file2,T);
 }
-
 
